adacfe27c7fd9ad1883886a7a1d5db751b1f2d73
nahashonkaroki <nahashon.karoki@gliffinglobaltech.com>
2024-11-17 13:56:37 +0300

    v1

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,17 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-export default defineConfig(({ command }) => ({
+export default defineConfig({
     plugins: [react()],
     build: {
+        outDir: 'dist', // The output directory for the build files
         rollupOptions: {
-            input: command === 'build' ? './src/main.tsx' : './src/entry-server.tsx'
-        }
-    },
-    ssr: {
-        noExternal: ['react', 'react-dom']
+            input: 'index.html', // Ensure Vite knows to use index.html as the entry point
+        },
     },
     resolve: {
         alias: {
-            '@': path.resolve(__dirname, './src')
-        }
-    }
-}));
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+});
